24fd26d4c5cf4371f7247599cd897da432fece54
Ville Tirronen <ville.e.t.tirronen@jyu.fi>
2011-03-11 13:06:54 +0200

    Added Cabal and License files along with lot of documentation

--- Numeric/Optimization/Algorithms/DifferentialEvolution.hs
+++ Numeric/Optimization/Algorithms/DifferentialEvolution.hs
@@ -1,5 +1,36 @@
-{-# LANGUAGE ScopedTypeVariables, ViewPatterns, BangPatterns, DeriveDataTypeable, RecordWildCards, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, ImpredicativeTypes, TypeFamilies, UndecidableInstances, TemplateHaskell #-}
-module Numeric.Optimization.Algorithms.DifferentialEvolution where
+{-# LANGUAGE ScopedTypeVariables, ViewPatterns, BangPatterns, DeriveDataTypeable, RecordWildCards, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, ImpredicativeTypes, TypeFamilies, UndecidableInstances, TemplateHaskell,TypeOperators #-}
+-- | Module    : Numeric.Optimization.Algorithms.DifferentialEvolution
+-- Copyright   : (c) 2011 Ville Tirronen
+-- License     : MIT
+--
+-- Maintainer  : ville.tirronen@jyu.fi
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This module implements basic version of Differential Evolution algorithm
+-- for finding minimum of possibly multimodal and non-differentiable real valued
+-- functions. 
+-- 
+-- Example
+-- >>>import Data.Vector.Unboxed as VUB
+--
+-- >>>import Numeric.Optimization.Algorithms.DifferentialEvolution
+-- 
+-- >>>let fitness = VUB.sum . VUB.map (*2)
+--  
+-- >>>de (defaultParams fitness ((VUB.replicate 60 0), (VUB.replicate 60 0)))
+-- (0.12486060253695,fromList [2.481036288296201e-3, ... ]
+--
+
+module Numeric.Optimization.Algorithms.DifferentialEvolution(
+        -- * Basic Types
+        Vector, Bounds, Fitness, Budget, DeMonad,
+        -- * Control Parameters
+        DEArgs(..), Strategy(..), strategy, defaultParams,
+        -- * Accessing internal state of the algorithm
+        evaluationCount, population, optimizationTrace,
+        -- * Executing the algorithm
+        runDE, de, deStep) where
 
 import qualified Control.Parallel.Strategies as CPS
 import Control.DeepSeq
@@ -19,7 +50,8 @@ import Control.Monad.Primitive
 import System.Random.MWC
 import Data.Word
 
-import Test.QuickCheck hiding (Gen)
+-- import Test.QuickCheck hiding (Gen)
+
 
 type Vector  = VUB.Vector Double
 type Bounds  = (VUB.Vector Double, VUB.Vector Double)
@@ -33,12 +65,24 @@ data DEParams s = DEParams {_gen :: GenST s
 
 $(mkLabels [''DEParams])
 
+-- |The current number of fitness evaluations
+evaluationCount :: forall s. DEParams s :-> Int
+evaluationCount = ec
+
+-- |The current set of active trial points
+population :: forall s. DEParams s :-> V.Vector (Double,Vector)
+population      = pop
+
+-- |The execution trace of current run
+optimizationTrace :: forall s. DEParams s :-> [(Int,Double,String)]
+optimizationTrace = trace
+
+-- |Monad for storing optimization trace and random number generator
 newtype DeMonad s a = DE (StateT (DEParams s) (ST s) a) deriving (Monad)
 
 instance MonadState (DEParams s) (DeMonad s) where
     get   = DE $ get
     put a = DE $ put a
-
 
 instance HasPRNG (DeMonad s) where
     type S (DeMonad s) = s
@@ -47,12 +91,14 @@ instance HasPRNG (DeMonad s) where
 liftST :: ST s a -> DeMonad s a
 liftST op = DE $ lift op
 
+-- |Extract values from the DeMonad.
 runDE :: (forall s. DeMonad s a) -> a
 runDE de = runST (let (DE a ) = de in evalStateT a 
                                        $ DEParams (error "Generator uninitialized") 
                                                   0
                                                   (error "Population unitialized") 
                                                   [])
+
 logPoint :: String -> DeMonad s ()
 logPoint tx = do
     (cb,_) <- getM pop >>= return . V.minimumBy (compare`on`fst)
@@ -87,7 +133,7 @@ expVariate' !lambda gen = work 0
                x :: Float <- uniform gen
                if x<lambda then work (n+1)
                            else return (n::Int)
- -- --
+-- --
 -- These should also have their own Vector - module
 (<+>),(<->) :: Vector -> Vector -> Vector
 a <+> b = VUB.zipWith (+) a b
@@ -98,7 +144,7 @@ a *|  b = VUB.map (a*) b
 -- -- --
 
 
--- These should go to their own module
+-- |Different strategies for optimization
 data Strategy  = Rand1Bin {cr ::{-# UNPACK #-} !Float, f ::{-# UNPACK #-} !Double}
                | Rand2Bin {cr ::{-# UNPACK #-} !Float, f ::{-# UNPACK #-} !Double}
                | Rand1Exp {cr ::{-# UNPACK #-} !Float, f ::{-# UNPACK #-} !Double} 
@@ -106,6 +152,7 @@ data Strategy  = Rand1Bin {cr ::{-# UNPACK #-} !Float, f ::{-# UNPACK #-} !Doubl
                deriving (Show)
 
 type DEStrategy s = GenST s -> Int -> Vector -> V.Vector (Double, Vector) -> DeMonad s Vector
+-- |Convert a Showable strategy into executable one
 strategy :: Strategy -> DEStrategy s
 strategy (Rand1Bin{..}) = strat' f cr rand1 binCrossover
 strategy (Rand2Bin{..}) = strat' f cr rand2 binCrossover
@@ -142,6 +189,7 @@ expCrossover l cr a b gen = do
        --                         forM_ ([0..overflow-1]++[index..end-1]) $ \i -> (MUB.write v i (b VUB.! i)))
        --                 a
 
+{-
 prop_r1_len s' l' = VUB.length (moduloReplacement1 s l dim a b) == dim
     where
      s = abs s' `mod` dim
@@ -169,6 +217,7 @@ prop_r1_eq3 s' l' = (moduloReplacement1 s l tdim tva tvb) ==
 tdim = 100
 tva   = VUB.replicate tdim (0::Int)
 tvb   = VUB.replicate tdim (1::Int)
+-}
 
 moduloReplacement1 start length dim a b 
     = VUB.modify (\v -> do 
@@ -213,15 +262,30 @@ binCrossover l cr a b gen = do
         return $ VUB.map (\(x,e1,e2,i) -> if x<cr || i == index then e2 else e1) 
                $ VUB.zip4 randoms a b (VUB.enumFromN 0 l)
 
+-- |Parameters for algorothm execution
 data DEArgs = DEArgs {
+                      -- |Mutation strategy
                       destrategy :: Strategy
+                      -- |N-dimensional function to be minimized
                      ,fitness     :: Fitness
-                     ,bounds      :: Maybe Bounds
+                      -- |N-orthope describing the domain of the fitness function
+                     ,bounds      :: Bounds
+                      -- |N, this should work well with dimension from 2-100
                      ,dim         :: Int
+                      -- |Number of indeviduals to use in optimization (60 is good)
                      ,spop        :: Int
+                      -- |Number of fitness function evaluations until termination
                      ,budget      :: Budget
+                      -- |Seed value for random number generation. (You often wish 
+                      --  to replicate results without storing)
                      ,seed        :: Seed}
 
+-- |Generate a parameter setting for DE. 
+defaultParams fitness bounds = DEArgs (Rand1Exp 0.9 0.70) 
+                                         fitness bounds dimension 
+                                         60 (5000*dimension) seed
+                        where seed = runST (create >>=save)
+                              dimension = VUB.length . fst $ bounds  
 saturateVector :: Bounds -> VUB.Vector Double -> VUB.Vector Double
 saturateVector (mn,mx) x = VUB.modify (\m -> go m (MUB.length m-1)) x
     where
@@ -236,6 +300,7 @@ saturateVector (mn,mx) x = VUB.modify (\m -> go m (MUB.length m-1)) x
 --saturate (lb,ub) x = min (max x lb) ub
 
 
+-- | Create a Differential Evolution process
 de :: DEArgs -> DeMonad s (Double,Vector)
 de DEArgs{..} = do
     liftST (restore seed) >>= setM gen
@@ -250,7 +315,9 @@ de DEArgs{..} = do
                 then getM pop >>= return . V.minimumBy (compare`on`fst)
                 else getM pop >>= deStep strat bounds fitness >>= setM pop >> work
 
-deStep :: DEStrategy s -> Maybe Bounds -> Fitness 
+-- | Single iteration of Differential Evolution. Could be an useful building block
+--   for other algorithms as well.
+deStep :: DEStrategy s -> Bounds -> Fitness 
           -> V.Vector (Double,Vector)
           -> DeMonad s (V.Vector (Double,Vector))  
 deStep strat bounds fitness pop = do 
@@ -262,6 +329,5 @@ deStep strat bounds fitness pop = do
         w <- strat gen l a pop
         return (select orig (postProcess w))
     select (fa,a) b@(fitness -> fb) = if fa < fb then (fa,a) else (fb,b) 
-    postProcess x | Just bds <-  bounds = saturateVector bds x
-                  | otherwise           = x
+    postProcess x = saturateVector bounds x
 
