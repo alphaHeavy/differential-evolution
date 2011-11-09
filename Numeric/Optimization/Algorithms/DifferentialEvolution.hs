0f72760c1e2e373348db0c8a247360ba2358b14f
Ville Tirronen <aleator@gmail.com>
2011-11-09 15:29:43 +0200

    Untested Monoidal interface for DE

--- Numeric/Optimization/Algorithms/DifferentialEvolution.hs
+++ Numeric/Optimization/Algorithms/DifferentialEvolution.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables, ViewPatterns, BangPatterns, DeriveDataTypeable, RecordWildCards, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, ImpredicativeTypes, TypeFamilies, UndecidableInstances, TemplateHaskell,TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables, ViewPatterns, BangPatterns, DeriveDataTypeable, RecordWildCards, GeneralizedNewtypeDeriving, MultiParamTypeClasses, RankNTypes, ImpredicativeTypes, TypeFamilies, UndecidableInstances, TemplateHaskell,TypeOperators, FlexibleContexts #-}
 -- |Module    : Numeric.Optimization.Algorithms.DifferentialEvolution
 -- Copyright   : (c) 2011 Ville Tirronen
 -- License     : MIT
@@ -11,13 +11,13 @@
 -- 
 module Numeric.Optimization.Algorithms.DifferentialEvolution(
         -- * Basic Types
-        Vector, Bounds, Fitness, Budget, DeMonad,
+        Vector, Bounds, Fitness, Budget, DeMonad, DEParams(..),
         -- * Control Parameters
         DEArgs(..), Strategy(..), strategy, defaultParams,
         -- * Accessing internal state of the algorithm
-        evaluationCount, population, optimizationTrace,
+        evaluationCount, population, currentBest,
         -- * Executing the algorithm
-        runDE, de, deStep) where
+        runDE, de, de', deStep) where
 
 import qualified Control.Parallel.Strategies as CPS
 import Control.DeepSeq
@@ -28,13 +28,17 @@ import qualified Data.Vector.Unboxed as VUB
 import qualified Data.Vector.Unboxed.Mutable as MUB
 
 import Data.Function
-import Data.Record.Label
+import Data.Label as L
+import Data.Label.PureM as M
 import Data.Monoid
 import Data.Ord
 import Control.Monad
-import Control.Arrow ((&&&))
 import Control.Monad.ST
-import Control.Monad.State
+import Control.Arrow ((&&&))
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State hiding (get,gets,modify)
+import qualified Control.Monad.State as ST (get,modify)
 import Control.Monad.Primitive
 import System.Random.MWC
 import Data.Word
@@ -53,72 +57,41 @@ type Budget = Int
 
 -- * Optimizer State
 
-data DEParams s w = DEParams {_gen :: GenST s
-                             ,_ec  :: Int
-                             ,_pop :: V.Vector (Double,Vector)
-                             ,_trace :: w}
+data DEParams = DEParams {_ec  :: Int
+                          ,_pop :: V.Vector (Double,Vector)}
 
 $(mkLabels [''DEParams])
 
 -- |The current number of fitness evaluations
-evaluationCount :: forall w. forall s. DEParams s w :-> Int
+evaluationCount :: DEParams :-> Int
 evaluationCount = ec
 
 -- |The current set of active trial points
-population :: forall w. forall s. DEParams s w :-> V.Vector (Double,Vector)
+population :: DEParams :-> V.Vector (Double,Vector)
 population = pop
-{-
+
 -- | Get the current best indevidual and Fitness
-currentBest :: forall w. forall s. DEParams s w :-> (Double,Vector)
+currentBest :: DEParams -> (Double,Vector)
 currentBest = V.minimumBy (comparing fst) . get pop
--}
--- |The execution trace of current run
-optimizationTrace :: (Monoid w) => forall s. DEParams s w :-> w
-optimizationTrace = trace
+
 
 -- * Context monad
 
 -- |Monad for storing optimization trace and random number generator
-newtype DeMonad s w a = DE (StateT (DEParams s w) (ST s) a) deriving (Monad)
+
+type DeMonad m gen w  = ReaderT gen (WriterT w m) 
+runDE :: (PrimMonad m) => DeMonad m (Generator m) w a -> (Generator m) -> m (a,w)
+runDE m g = runWriterT (runReaderT m g)
+
+
+
 
 type Generator m = Gen (PrimState m)
 
-instance MonadState (DEParams s w) (DeMonad s w) where
-    get   = DE $ get
-    put a = DE $ put a
-
-instance HasPRNG (DeMonad s w) where
-    type S (DeMonad s w) = s
-    withGen op = get >>= \x -> op (_gen x)
-
-liftST :: ST s a -> DeMonad s w a
-liftST op = DE $ lift op
-
--- |Extract values from the DeMonad.
-runDE :: Monoid w => (forall s. DeMonad s w a) -> a
-runDE de = runST (let (DE a ) = de in evalStateT a 
-                                       $ DEParams (error "Generator uninitialized") 
-                                                  0
-                                                  (error "Population unitialized") 
-                                                  mempty)
-
-logPoint :: String -> DeMonad s w ()
-logPoint tx = do
-    (cb,_) <- getM pop >>= return . V.minimumBy (compare`on`fst)
-    e <- getM ec
-    return ()
---    modM trace ((e,cb,tx):)
-
-
--- HasPRNG related
-class HasPRNG m where
-    type S m :: *
-    withGen :: (Gen (S m) -> m b) -> m b
-
 selectRandom :: (PrimMonad m) => Gen (PrimState m) -> Int -> V.Vector a -> m [a]
 selectRandom !gen !n vec = do
-    let idx = replicate n 0
-    --idx <- replicateM n (randomIndex (V.length vec) gen)
+    --let idx = replicate n 0
+    idx <- replicateM n (randomIndex (V.length vec) gen)
     return $ map (V.unsafeIndex vec) idx
 
 {-#INLINE randomIndex#-}
@@ -149,17 +122,18 @@ data Strategy  = Rand1Bin {cr ::{-# UNPACK #-} !Float, f ::{-# UNPACK #-} !Doubl
                | Rand2Exp {cr ::{-# UNPACK #-} !Float, f ::{-# UNPACK #-} !Double} 
                deriving (Show)
 
-type DEStrategy s w = GenST s -> Int -> Vector -> V.Vector (Double, Vector) -> DeMonad s w Vector
+type DEStrategy m w = Generator m -> Int -> Vector -> V.Vector (Double, Vector) 
+                        -> DeMonad m (Generator m) w Vector
 
 -- |Convert a Showable strategy into executable one
-strategy :: Strategy -> DEStrategy s w
+strategy :: (PrimMonad m, Monoid w) => Strategy -> DEStrategy m w
 strategy (Rand1Bin{..}) = strat' f cr rand1 binCrossover
 strategy (Rand2Bin{..}) = strat' f cr rand2 binCrossover
 strategy (Rand1Exp{..}) = strat' f cr rand1 expCrossover
 strategy (Rand2Exp{..}) = strat' f cr rand2 expCrossover 
 {-# INLINE strategy #-}
 
-strat' f cr m co =  \gen l parent pop -> liftST (m gen f pop >>= \x -> co l cr parent x gen)
+strat' f cr m co =  \gen l parent pop -> (lift . lift) (m gen f pop >>= \x -> co l cr parent x gen)
 
 
 rand1 :: (PrimMonad m) => Generator m -> Double -> V.Vector (Double,Vector) -> m Vector
@@ -206,7 +180,7 @@ binCrossover l cr a b gen = do
                $ VUB.zip4 randoms a b (VUB.enumFromN 0 l)
 
 -- |Parameters for algorithm execution
-data DEArgs = DEArgs {
+data DEArgs w = DEArgs {
                       -- |Mutation strategy
                       destrategy :: Strategy
                       -- |N-dimensional function to be minimized
@@ -225,8 +199,7 @@ data DEArgs = DEArgs {
                       -- |Tracing function. You can use this to track parameters of the evolution.
                       --  (Ie. This is how you access the final fitness, result vector and fitness 
                       --  trace)
-                     ,trace       :: (Monoid w) -> (forall s. DEParams s w) -> w
-                     
+                     ,trace       :: (Monoid w) => DEParams -> w
                      }
 
 -- |Generate a parameter setting for DE. 
@@ -244,38 +217,45 @@ saturateVector (mn,mx) x = VUB.modify (\m -> go m (MUB.length m-1)) x
               xi <- MUB.read x i
               when (xi < (mn VUB.! i)) $  MUB.write x i (mn VUB.! i)
               when (xi > (mn VUB.! i)) $  MUB.write x i (mx VUB.! i)
-   
--- | Create a Differential Evolution process
-de :: (Monoid w) => DEArgs -> DeMonad s w (Double,Vector)
-de DEArgs{..} = do
-    liftST (restore seed) >>= setM gen
-    
-    init <- withGen $ \g -> liftST (V.replicateM spop (uniformVector g dim >>= return.scale)) 
-    pop =: V.map (fitness &&& id) init
-    work
+
+-- | Run DE algorithm over a 'PrimMonad' such as 'IO' or 'ST'. Returns the fitness trace 
+--   specified by 'DEArgs.trace'
+de :: (Monoid w, PrimMonad m) => DEArgs w -> Seed -> m w 
+de args seed = restore seed >>= runDE (de' args) >>= return . snd
+
+-- | Create a Differential Evolution process inside DeMonad
+de' :: (Monoid w, PrimMonad m) => DEArgs w -> DeMonad m (Generator m) w ()
+de' DEArgs{..} = do
+    gen  <- lift . lift $ restore seed
+    init <- lift . lift $ V.replicateM spop (uniformVector gen dim >>= return.scale) 
+    let population =  V.map (fitness &&& id) init
+        state      = DEParams (V.length population) population
+    work gen state
     where
      (lb,ub) = (fst bounds, snd bounds)
      scale x = VUB.zipWith3 (\l u x -> l+x*(u-l)) lb ub x
      strat = strategy destrategy
-     work = do logPoint ""
-               e <- getM ec 
-               if e > budget 
-                then getM pop >>= return . V.minimumBy (compare`on`fst)
-                else getM pop >>= deStep strat bounds fitness >>= setM pop >> work
+     work gen state = do 
+               lift . tell $ trace state
+               if get ec state > budget 
+                then return () 
+                else deStep strat bounds fitness gen state >>= work gen
 
 -- | Single iteration of Differential Evolution. Could be an useful building block
 --   for other algorithms as well.
-deStep :: (Monoid w) =>
-          DEStrategy s w -> Bounds -> Fitness 
-          -> V.Vector (Double,Vector)
-          -> DeMonad s w (V.Vector (Double,Vector))  
-deStep strat bounds fitness pop = do 
-        modM ec (+V.length pop)
-        withGen $ \g -> (V.mapM (candidate g) pop) 
+deStep :: (Monoid w, PrimMonad m) =>
+          DEStrategy m w -> Bounds -> Fitness 
+          -> Generator m
+          -> DEParams -- V.Vector (Double,Vector)
+          -> DeMonad m (Generator m) w DEParams -- (V.Vector (Double,Vector))  
+deStep strat bounds fitness gen params@(get pop -> population) = do 
+        let newEc = get ec params + V.length population
+        newPop <- (V.mapM (candidate gen) population) 
+        return $ set ec newEc . set pop newPop $ params 
    where 
-    l = VUB.length . snd . V.head $ pop
+    l = VUB.length . snd . V.head $ population
     candidate gen orig@(ft,a) = do
-        w <- strat gen l a pop
+        w <- strat gen l a population
         return (select orig (postProcess w))
     select (fa,a) b@(fitness -> fb) = if fa < fb then (fa,a) else (fb,b) 
     postProcess x = saturateVector bounds x
